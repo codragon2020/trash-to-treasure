f6d0458f79e227603ac706c959a2e66005e60458
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-04-20 07:00:21 -0400

    Adding Mongoose config.

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const path = require("path");
+const connectDB = require("./config/db")
+
+connectDB()
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -10,10 +14,10 @@ if (process.env.NODE_ENV === "production") {
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
