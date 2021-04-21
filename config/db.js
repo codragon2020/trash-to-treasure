ef1e4d66d4ca1e019b4ec197fd907cd44c6d4e5c
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-04-20 21:20:15 -0400

    Updated Mongoose config from server.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,13 @@
-import mongoose from 'mongoose';
+// demo config not working
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useUnifiedTopology = true,
             useNewUrlParser: true,
-            useCreateIndex: true
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true,
         })
 
         console.log(`MongoDB Connected: ${conn.connection.host}`)
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const path = require("path");
-const connectDB = require("./config/db")
+// demo config - not working
+// const connectDB = require("./config/db")
+// connectDB()
 
-connectDB()
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -11,6 +13,23 @@ const app = express();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/t2t", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+});
+
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+    console.log("Mongoose connected successfully.");
+});
+
+connection.on("error", (err) => {
+    console.log("Mongoose connection error: " + err);
+});
 
 // Send every request to the React app
 // Define any API routes before this runs
