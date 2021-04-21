6617963ae097d83456b9a99600da0f8dfc75d04a
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-04-20 21:41:06 -0400

    Installed and configured Colors and Morgan packages for logging

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const path = require("path");
 // demo config - not working
 // const connectDB = require("./config/db")
 // connectDB()
-
+const colors = require("colors");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// Concise output colored by response status for development use. The :status token will be colored green for success codes, red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for information codes.
+app.use(logger("dev"));
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -24,11 +28,11 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/t2t", {
 const connection = mongoose.connection;
 
 connection.on("connected", () => {
-    console.log("Mongoose connected successfully.");
+    console.log("Mongoose connected successfully.".cyan.bold);
 });
 
 connection.on("error", (err) => {
-    console.log("Mongoose connection error: " + err);
+    console.log("Mongoose connection error: " + err.red.bold);
 });
 
 // Send every request to the React app
@@ -38,5 +42,5 @@ app.get("*", function (req, res) {
 });
 
 app.listen(PORT, function () {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+  console.log(`🌎 ==> API server now on port ${PORT}!`.yellow.bold);
 });
