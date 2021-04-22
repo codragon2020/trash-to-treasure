c1463ed169fcdb5666f3cc66ac223b30c46d4ba7
Daniel-Edwards56 <73670462+Daniel-Edwards56@users.noreply.github.com>
2021-04-21 20:40:04 -0400

    setting up basic react front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Header />
+        <main className="py-3">
+          <Container>
+            <h1>Hello World!</h1>
+            <p>Welcome to Trash to Treasure!</p>
+          </Container>
+        </main>
+        <Footer />
       </div>
     );
   }
