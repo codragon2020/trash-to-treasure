17151eaa9fcc6dd90f3ec065e66acc2130ca6531
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-04-21 21:37:19 -0400

    Adding seeder.js data

---
+++ data/seeder.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const users = require("./users.js");
+const products = require("./products.js");
+const User = require("../models/userModel.js");
+const Product = require("../models/productModel.js");
+const Order = require("../models/orderModel.js");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/t2t");
+
+const importData = async () => {
+  try {
+    await Order.deleteMany()
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    const createdUsers = await User.insertMany(users);
+
+    const adminUser = createdUsers[0]._id;
+
+    const sampleProducts = products.map((product) => {
+      return { ...product, user: adminUser };
+    });
+
+    await Product.insertMany(sampleProducts);
+
+    console.log("Data Imported!".green.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(`${error}`.red.inverse);
+    process.exit(1);
+  }
+};
+
+const destroyData = async () => {
+  try {
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    console.log("Data Destroyed!".red.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(`${error}`.red.inverse);
+    process.exit(1);
+  }
+};
+
+if (process.argv[2] === "-d") {
+    importData();
+} else {
+  destroyData();
+}
--- models/productModel.js
+++ models/productModel.js
@@ -40,4 +40,4 @@ const productSchema = mongoose.Schema(
 
 const Product = mongoose.model("Product", productSchema);
 
-export default Product;
+module.exports = Product;
