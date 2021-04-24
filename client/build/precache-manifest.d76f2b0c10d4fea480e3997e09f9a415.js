2be6367db7e3b35607a6d8583073c4dbdb8b038c
binayaluitel2 <binayababuluitel@gmail.com>
2021-04-24 00:26:08 -0400

    Fetching Products

---
+++ client/build/precache-manifest.d76f2b0c10d4fea480e3997e09f9a415.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4459732a4d838eedf04",
+    "url": "/static/css/main.7f866db9.chunk.css"
+  },
+  {
+    "revision": "e4459732a4d838eedf04",
+    "url": "/static/js/main.7e925a4e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4c24df426030c9230b7b",
+    "url": "/static/js/2.175a8292.chunk.js"
+  },
+  {
+    "revision": "39d99ff441949b87d606f2354239dcdf",
+    "url": "/index.html"
+  }
+];
