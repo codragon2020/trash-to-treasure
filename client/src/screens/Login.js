8dd69dcfde1889e881280b36f8730762170b3cc5
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-04-27 06:37:48 -0400

    Added Login.js page and Bootstrap cdn

---
+++ client/src/screens/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Login() {
+    return (
+        <div className="Login">
+            <div className = "col-md-6 offset-md-3 text-center">
+                <h1>Login with Google</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
