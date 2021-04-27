fbcec9836827a82ee6a9b6669e9c9545d8437101
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-04-27 07:45:59 -0400

    Installed dependencies for react-google-login and associated the Google Login clientId in the Login page

--- client/src/screens/Login.js
+++ client/src/screens/Login.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React from "react";
+import { GoogleLogin } from "react-google-login";
+
 
 function Login() {
+    
+    const responseSuccessGoogle = (response) => {
+        console.log(response);
+    }
+
+    const responseErrorGoogle = (response) => {
+        
+    }
+
     return (
-        <div className="Login">
-            <div className = "col-md-6 offset-md-3 text-center">
-                <h1>Login with Google</h1>
-            </div>
-        </div>
-    )
+    <div className="Login">
+      <div className="col-md-6 offset-md-3 text-center">
+        <h1>Login with Google</h1>
+        <GoogleLogin
+          clientId="1084092224337-7lgj0bui87cdc7ftptrpu00skccbq2pm.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={responseSuccessGoogle}
+          onFailure={responseErrorGoogle}
+          cookiePolicy={"single_host_origin"}
+        />
+        ,
+      </div>
+    </div>
+  );
 }
 
 export default Login;
