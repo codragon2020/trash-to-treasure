13e41a191d6c638bf6f860f923f6f7816ecf474c
Daniel-Edwards56 <73670462+Daniel-Edwards56@users.noreply.github.com>
2021-05-02 17:12:36 -0400

    finished create product page without backend connection

--- client/src/screens/CreateProduct.js
+++ client/src/screens/CreateProduct.js
@@ -1,15 +1,4 @@
 import React from "react";
-// import { Link } from "react-router-dom";
-// import {
-//   Form,
-//   Input,
-//   TextArea,
-//   Button,
-//   Image,
-//   Message,
-//   Header,
-//   Icon
-// } from "react-bootstrap";
 import {
   Form,
   Input,
@@ -21,28 +10,51 @@ import {
   Icon
 } from "semantic-ui-react";
 
+const INITIAL_PRODUCT = {
+  name: "",
+  price: "",
+  media: "",
+  description: "",
+  contactName: "",
+  contactNumber: "",
+  contactEmail: ""
+};
+
 function CreateProduct() {
-  const [product, setProduct] = React.useState({
-    name: "",
-    price: "",
-    media: "",
-    description: "",
-    contactName: "",
-    contactNumber: "",
-    contactEmail: ""
-  });
+  const [product, setProduct] = React.useState(INITIAL_PRODUCT);
+  const [mediaPreview, setMediaPreview] = React.useState("");
+  const [success, setSuccess] = React.useState(false);
+
   function handleChange(event) {
-    const { name, value } = event.target;
-    setProduct({ [name]: value });
-    console.log(product);
+    const { name, value, files } = event.target;
+    if (name === "media") {
+      setProduct(prevState => ({ ...prevState, media: files[0] }));
+      setMediaPreview(window.URL.createObjectURL(files[0]));
+    } else {
+      setProduct(prevState => ({ ...prevState, [name]: value }));
+    }
   }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(product);
+    setProduct(INITIAL_PRODUCT);
+    setSuccess(true);
+  }
+
   return (
     <>
       <Header as="h2" block>
         <Icon name="add" color="orange" />
         Create New Product
       </Header>
-      <Form>
+      <Form success={success} onSubmit={handleSubmit}>
+        <Message
+          success
+          icon="check"
+          header="Success!"
+          content="Your product has been posted"
+        />
         <Form.Group widths="equal">
           <Form.Field
             control={Input}
@@ -72,42 +84,48 @@ function CreateProduct() {
             content="Select Image"
             onChange={handleChange}
           />
-          <Form.Field
-            control={TextArea}
-            name="description"
-            label="Desciption"
-            placeholder="Desciption"
-            onChange={handleChange}
-          />
-          <Form.Field
-            control={Input}
-            name="contactName"
-            label="ContactName"
-            placeholder="Contact Name"
-            onChange={handleChange}
-          />
-          <Form.Field
-            control={Input}
-            name="contactNumber"
-            label="ContactNumber"
-            placeholder="Contact Number"
-            onChange={handleChange}
-          />
-          <Form.Field
-            control={Input}
-            name="contactEmail"
-            label="ContactEmal"
-            placeholder="Contact Email"
-            onChange={handleChange}
-          />
-          <Form.Field
-            control={Button}
-            color="blue"
-            icon="pencil alternate"
-            content="Submit"
-            type="submit"
-          />
         </Form.Group>
+        <Image src={mediaPreview} rounded centered size="small" />
+        <Form.Field
+          control={Input}
+          name="contactName"
+          label="ContactName"
+          placeholder="Contact Name"
+          value={product.contactName}
+          onChange={handleChange}
+        />
+        <Form.Field
+          control={Input}
+          name="contactNumber"
+          label="ContactNumber"
+          placeholder="Contact Number"
+          value={product.contactNumber}
+          onChange={handleChange}
+        />
+        <Form.Field
+          control={Input}
+          name="contactEmail"
+          label="ContactEmal"
+          placeholder="Contact Email"
+          value={product.contactEmail}
+          onChange={handleChange}
+        />
+        <Form.Field
+          control={TextArea}
+          name="description"
+          label="Desciption"
+          placeholder="Desciption"
+          value={product.description}
+          onChange={handleChange}
+        />
+
+        <Form.Field
+          control={Button}
+          color="blue"
+          icon="pencil alternate"
+          content="Submit"
+          type="submit"
+        />
       </Form>
     </>
   );
