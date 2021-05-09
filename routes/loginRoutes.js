85d111474c4c4d364b6c4d721ee53e1de3e6ee30
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-05-09 07:38:30 -0400

    Adding backend changes for google route

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,4 +1,5 @@
-const router = require("express").Router()
+const router = require("express").Router();
+
 const cors = require("cors");
 const passport = require("passport");
 const passortLocal = require("passport-local").Strategy;
@@ -10,47 +11,53 @@ const User = require("../models/userModel");
 
 // Routes
 router.post("/login", (req, res, next) => {
-    passport.authenticate("local", (err, user, info) => {
-      if (err) throw err;
-      if (!user) res.status(500).send("No User Exists")
-      else {
-        req.login(user, (err) => {
-          if (err) throw err;
-          res.json({msg:"Successfully Authenticated", user:req.user});
-          console.log(req.user);
-        });
-      }
-    })(req, res, next);
-  });
-  router.post("/register", (req, res) => {
-    console.log(req.body);
-    User.findOne({ username: req.body.username }, async (err, doc) => {
-      if (err) throw err;
-      if (doc) res.send("User Already Exists");
-      if (!doc) {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-  
-        const newUser = new User({
-          username: req.body.username,
-          password: hashedPassword,
-        });
-        await newUser.save();
-        res.send("User Created");
-      }
-    });
-  });
+  passport.authenticate("local", (err, user, info) => {
+    if (err) throw err;
+    if (!user) res.status(500).send("No User Exists");
+    else {
+      req.login(user, (err) => {
+        if (err) throw err;
+        res.json({ msg: "Successfully Authenticated", user: req.user });
+        console.log(req.user);
+      });
+    }
+  })(req, res, next);
+});
+router.post("/register", (req, res) => {
+  console.log(req.body);
+  User.findOne({ username: req.body.username }, async (err, doc) => {
+    if (err) throw err;
+    if (doc) res.send("User Already Exists");
+    if (!doc) {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
-  router.get('/logout', function(req, res){
-    req.logout();
-    res.send("Logging Out User")
+      const newUser = new User({
+        username: req.body.username,
+        password: hashedPassword,
+      });
+      await newUser.save();
+      res.send("User Created");
+    }
   });
+});
 
-  router.get("/user", (req, res) => {
-      if(req.user) {res.json(req.user)}
-        else {
-            res.status(500).send("User not logged in")
-        }
-  });
+router.get("/logout", function (req, res) {
+  req.logout();
+  res.send("Logging Out User");
+});
 
+router.get("/user", (req, res) => {
+  if (req.user) {
+    res.json(req.user);
+  } else {
+    res.status(500).send("User not logged in");
+  }
+});
 
-module.exports = router
+// Google routes
+router.get('/google', (req, res) => {
+  //handle with passport
+  res.send('logging in with google');
+})
+
+module.exports = router;
