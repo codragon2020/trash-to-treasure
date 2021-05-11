91812392cdd1d808470faba76cc8ec331cdae09b
Daniel-Edwards56 <73670462+Daniel-Edwards56@users.noreply.github.com>
2021-05-10 21:32:35 -0400

    csschanges

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,28 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Card } from 'react-bootstrap'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { Card } from "react-bootstrap";
+import {} from "./style.css";
 const Product = ({ product }) => {
   return (
-    <Card className='my-3 p-3 rounded'>
+    <Card className="my-3 p-3 rounded cardz">
       <Link to={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant='top' />
+        <Card.Img className="picz" src={product.image} variant="top" />
       </Link>
 
       <Card.Body>
         <Link to={`/product/${product._id}`}>
-          <Card.Title as='div'>
+          <Card.Title as="div">
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
 
-        <Card.Text as='div'>
-        </Card.Text>
+        <Card.Text as="div"></Card.Text>
 
-        <Card.Text as='h3'>${product.price}</Card.Text>
+        <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
     </Card>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
